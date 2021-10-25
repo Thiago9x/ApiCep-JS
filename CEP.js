c47532bd875c8ba4cabc478c0376b400f33112e8
Thiago9x <thimendonca44@gmail.com>
2021-10-25 16:05:20 -0300

    api do cep completa

--- CEP.js
+++ CEP.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const pesquisarCep = async (cep) =>{
+const pesquisarCep = async (cep) => {
     const url = `https://viacep.com.br/ws/${cep}/json/`;
     const response = await fetch(url);
     const data = await response.json();
@@ -9,36 +9,38 @@ const pesquisarCep = async (cep) =>{
     return data;
 };
 
-const cepValido=(cep) => /^[0-9]{8}$/.test(cep);
+const cepValido = (cep) => /^[0-9]{8}$/.test(cep);
 
 
-const limparFormulario = () =>{
+const limparFormulario = () => {
     document.querySelector("#endereco").value = '';
     document.querySelector("#bairro").value = '';
     document.querySelector("#cidade").value = '';
     document.querySelector("#estado").value = '';
 }
-const preencherFormulario = async (evento)=>{
+const preencherFormulario = async (evento) => {
     // const cep = document.querySelector('#cep').value;
-    const cep = evento.target.value;
+    const cep = evento.target.value.replace('-', '');
     limparFormulario();
-    
-    if(cepValido(cep)){
-    const infoCep = await pesquisarCep(cep);
-   
-    document.querySelector("#endereco").value = infoCep.logradouro;
-    document.querySelector("#bairro").value = infoCep.bairro;
-    document.querySelector("#cidade").value = infoCep.localidade;
-    document.querySelector("#estado").value = infoCep.uf;
-    }
-    else{
+
+    if (cepValido(cep)) {
+        const infoCep = await pesquisarCep(cep);
+        if (infoCep.erro) {
+            document.querySelector('#endereco').value = 'CEP não encontrado!';
+        } else {
+            document.querySelector("#endereco").value = infoCep.logradouro;
+            document.querySelector("#bairro").value = infoCep.bairro;
+            document.querySelector("#cidade").value = infoCep.localidade;
+            document.querySelector("#estado").value = infoCep.uf;
+        }
+    } else {
         document.querySelector("#endereco").value = "CEP Incorreto!!!"
     }
 
-    
+
 }
 
 
 
 document.querySelector('#cep')
-.addEventListener('focusout', preencherFormulario);
+    .addEventListener('focusout', preencherFormulario);
